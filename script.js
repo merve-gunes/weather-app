b5558721ba3b2b0b32a226fd4e9d10caf6a4d6ed
Merve Gunes <mervegns283@gmail.com>
2026-03-02 00:47:02 +0300

    Capture city input value on button click

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ const weatherResult = document.getElementById("weatherResult");
 searchBtn.addEventListener("click" , () => {
 
 console.log("butona tıklandı..");
+
+const city = cityInput.value;
+console.log(city);
 });
