43fdaa4283fbbf76d78c6e88c26510d644ac3e96
Merve Gunes <mervegns283@gmail.com>
2026-03-02 00:31:28 +0300

    Initialize DOM elements and add click event listener

---
+++ script.js
@@ -0,0 +1,8 @@
+const cityInput = document.getElementById("cityInput")
+const searchBtn = document.getElementById("searchBtn");
+const weatherResult = document.getElementById("weatherResult");
+
+searchBtn.addEventListener("click" , () => {
+
+console.log("butona tıklandı..");
+});
