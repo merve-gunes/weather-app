59053aafe26a4c0522811339538997080f30b6ca
Merve Gunes <mervegns283@gmail.com>
2026-03-03 00:14:00 +0300

    secure API key with config and add gitignore

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-const cityInput = document.getElementById("cityInput")
+const cityInput = document.getElementById("cityInput");
 const searchBtn = document.getElementById("searchBtn");
 const weatherResult = document.getElementById("weatherResult");
 
-searchBtn.addEventListener("click" , () => {
 
-console.log("butona tıklandı..");
+const apiKey = config.API_KEY; 
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 
-const city = cityInput.value;
-console.log(city);
+searchBtn.addEventListener("click", () => {
+    const city = cityInput.value.trim(); 
+
+    if (city) {
+        getWeatherData(city);
+    } else {
+        alert("Lütfen bir şehir ismi giriniz!");
+    }
 });
+
+function getWeatherData(city) {
+    const url = `${baseUrl}?q=${city}&appid=${apiKey}&units=metric&lang=tr`;
+    console.log(url);
+    console.log("şehir:",city);
+
+    weatherResult.innerHTML = `<p>Veriler getiriliyor...<p>`;
+
+fetch(url)
+    .then(response => {
+            if (!response.ok) {
+                throw new Error("Şehir bulunamadı");
+            }
+            return response.json();
+        })
+    .then(data => {
+        console.log("DATA:", data);
+
+        weatherResult.innerHTML = `
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <p>🌡 Sıcaklık: ${data.main.temp}°C</p>
+        <p>🤒 Hissedilen: ${data.main.feels_like}°C</p>
+        <p>💧 Nem: ${data.main.humidity}%</p>
+        <p>🌬 Rüzgar: ${data.wind.speed} m/s</p>
+        <p>☁ Hava Durumu: ${data.weather[0].description}</p>
+    `;
+    })
+    .catch(error => {
+            weatherResult.innerHTML = `<p style="color: red;">⚠️ ${error.message}</p>`;
+        });
+}
