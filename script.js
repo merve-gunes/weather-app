1589b162f50059ce6096e6c8bf43eac9fa580758
Merve Gunes <mervegns283@gmail.com>
2026-03-04 00:43:34 +0300

    feat: add dynamic background change based on weather conditions

--- script.js
+++ script.js
@@ -38,8 +38,37 @@ fetch(url)
     .then(data => {
         console.log("DATA:", data);
 
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    
+        const weatherMain = data.weather[0].main; 
+        const body = document.body;
+
+        switch (weatherMain) {
+            case "Clouds":
+                body.style.background = "linear-gradient(to bottom, #bdc3c7, #2c3e50)"; 
+                break;
+            case "Clear":
+                body.style.background = "linear-gradient(to bottom, #f7b733, #fc4a1a)"; 
+                break;
+            case "Rain":
+            case "Drizzle":
+                body.style.background = "linear-gradient(to bottom, #4b6cb7, #182848)"; 
+                break;
+            case "Snow":
+                body.style.background = "linear-gradient(to bottom, #e6dada, #274046)"; 
+                break;
+            case "Thunderstorm":
+                body.style.background = "linear-gradient(to bottom, #0f0c29, #302b63, #24243e)"; 
+                break;
+            default:
+                body.style.background = "linear-gradient(to right, #6a11cb, #2575fc)"; 
+        }
+
         weatherResult.innerHTML = `
+    
         <h2>${data.name}, ${data.sys.country}</h2>
+        <img src="${iconUrl}" alt="${data.weather[0].description}">
         <p>🌡 Sıcaklık: ${data.main.temp}°C</p>
         <p>🤒 Hissedilen: ${data.main.feels_like}°C</p>
         <p>💧 Nem: ${data.main.humidity}%</p>
